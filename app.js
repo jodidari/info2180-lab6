7649cb02af8921a4fdee9aa22b77f0c0da6fdf03
Jodi Darien <jodydarien@yahoo.com>
2017-10-30 17:57:18 +0000

    Exercise 3

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 window.onload=function(){
-     let search=document.getElementById("search");
-     search.addEventListener("click",function(){
-         let request = new XMLHttpRequest();
-         request.onreadystatechange = function(){
-             if(request.readyState === 4 && request.status === 200){
-                 alert(request.responseText);
-             }
-         };
-         request.open("GET","request.php?q=definition",true);
-         request.send();
-});
+    let search=document.getElementById("search");
+    let result=document.getElementById("result");
+    search.addEventListener("click",function(){
+        let request = new XMLHttpRequest();
+        request.onreadystatechange = function(){
+            if(request.readyState === 4 && request.status === 200){
+                result.innerHTML="<h3>Result</h3>"+request.responseText;
+            }
+        };
+        request.open("GET","request.php?q="+document.getElementById("lookup").value,true);
+        request.send();
+    });
 };
